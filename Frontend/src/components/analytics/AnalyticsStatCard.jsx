244d266b4d3546b240451cd2ca39e1e6ee051dc3
RJ-4803 <renujaan4803@gmail.com>
2026-05-30 21:11:48 +0530

    fix: bug fixing

--- Frontend/src/components/analytics/AnalyticsStatCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import StatCard from '../common/StatCard';
-
-const AnalyticsStatCard = (props) => {
-  return <StatCard {...props} />;
-};
-
-export default AnalyticsStatCard;
